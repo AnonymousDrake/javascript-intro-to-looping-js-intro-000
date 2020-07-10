550b02d8a862d3decfa80af3758fb083131139ab
Ekambeer Singh <akashdeep0406@gmail.com>
2020-07-10 07:24:33 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -5,10 +5,22 @@ var forLoop= array =>{
   }
   return array;
 }
+
 var whileLoop= n =>{
   while(n!=0){
     console.log(n);
     n-=1;
   }
   return 'done';
+}
+
+let i=0;
+var incrementVariable= () =>{
+  i+=1;
+  return i;
+}
+var doWhileLoop= n =>{
+  do{
+    console.log(`I run once regardless.`);
+  }while(incrementVariable()<n)
 }
