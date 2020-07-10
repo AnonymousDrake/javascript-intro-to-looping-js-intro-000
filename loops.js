870f0bed30c958e3fface42c54eb7e3fa16771ec
Ekambeer Singh <akashdeep0406@gmail.com>
2020-07-10 06:58:00 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,5 @@
 var forLoop= (array) =>{
-  let a=array.length();
-  for(let i=a;i<=a+25;i++)
+  for(let i=0;i<=25;i++)
   {
     if(i===0||i===1) array[i]=`I am ${i} strange loop.`;
     else array[i]=`I am ${i} strange loop.`;
