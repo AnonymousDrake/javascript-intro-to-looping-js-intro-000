b9b1fe8877cb6fb812a86dc6756095100aedaea0
Ekambeer Singh <akashdeep0406@gmail.com>
2020-07-10 07:12:58 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 var forLoop= array =>{
-  for(let i=0; i<=25; i+=1){
+  for(let i=0; i<25; i+=1){
     if(i===1) {array.push(`I am 1 strange loop.`)}
     else {array.push(`I am ${i} strange loops.`)}
   }
