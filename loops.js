4689dd5fd3d146adafcbe50b15cd8a2d7b47ab06
Ekambeer Singh <akashdeep0406@gmail.com>
2020-07-10 06:55:52 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,7 @@
 var forLoop= (array) =>{
   for(let i=arraylength();i<=25;i++)
   {
-    array[i]=`I am ${i} strange loops`;
+    if(i===0||i===1) array[i]=`I am ${i} strange loop`;
+    else array[i]=`I am ${i} strange loop`;
   }
 }
